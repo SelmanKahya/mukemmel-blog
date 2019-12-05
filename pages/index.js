6c5b35e9f2ff7b8d5eb3e44db0b0f0751ef48922
Selman K <selmankahya@hotmail.com>
2019-12-05 01:13:58 -0800

    use heroku url

--- pages/index.js
+++ pages/index.js
@@ -80,7 +80,7 @@ const Home = ({ posts }) => (
 
 Home.getInitialProps = async ({ req }) => {
   // TODO: aşağıdaki satırda bulunan adresi kendi sunucu adresinle değiştirmelisin
-  const res = await fetch("http://localhost:3000/api/posts");
+  const res = await fetch("https://mukemmel-blog.herokuapp.com/api/posts");
   const json = await res.json();
   return { posts: json.posts };
 };
