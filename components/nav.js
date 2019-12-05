2a3c2c624954922a7976b344b90ea86fd0c41074
rıdvan <ridvanozcan48@hotmail.com>
2019-12-05 14:34:16 +0300

    Update nav.js
    Deneme amaçlı il düzenlememi yaptım.Devamı gelecek

--- components/nav.js
+++ components/nav.js
@@ -14,7 +14,7 @@ const Nav = () => (
     <ul>
       <li>
         <Link href="/">
-          <a>Home</a>
+          <a>Anasayfa</a>
         </Link>
       </li>
       {links.map(({ key, href, label }) => (
