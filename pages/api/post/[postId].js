7653d1dd7dcb0689383c60cd957b3804e4a00b47
Selman K <selmankahya@hotmail.com>
2019-12-05 01:22:35 -0800

    return not found

--- pages/api/post/[postId].js
+++ pages/api/post/[postId].js
@@ -3,7 +3,15 @@ import { getPosts } from "../../../src/blog-posts";
 const posts = getPosts();
 
 export default (req, res) => {
+  const post = posts.find(post => post.slug === req.query.postId);
+  if (!post) {
+    return res.json({
+      post: {
+        title: "NOT FOUND 404"
+      }
+    });
+  }
   res.json({
-    post: posts.find(post => post.slug === req.query.postId)
+    post
   });
 };
