936b7191d058bc994869d96a9f72371aecde4f96
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2019-12-06 04:25:03 +0100

    throw an error when the post does not exists

--- pages/[postId].js
+++ pages/[postId].js
@@ -2,27 +2,44 @@ import React from "react";
 import fetch from "isomorphic-unfetch";
 import Page from "../layouts/main";
 import SinglePost from "../components/singlePost";
+import Error from "next/error";
 
-const BlogPost = ({ post }) => (
-  <Page minimal={true}>
-    <div class="container mx-auto w-8/12">
-      <SinglePost
-        detailsPage={true}
-        title={post.title}
-        details={post.details}
-        userName={post.user.name}
-        userImage={post.user.profile_picture}
-        date={post.date}
-      />
-    </div>
-  </Page>
-);
+const BlogPost = ({ post, statusCode }) => {
+  if (statusCode === 404) {
+    return <Error statusCode={statusCode} />;
+  }
+  return (
+    <Page minimal={true}>
+      <div class="container mx-auto w-8/12">
+        <SinglePost
+          detailsPage={true}
+          title={post.title}
+          details={post.details}
+          userName={post.user.name}
+          userImage={post.user.profile_picture}
+          date={post.date}
+        />
+      </div>
+    </Page>
+  );
+};
 
-BlogPost.getInitialProps = async ({ req, query }) => {
+BlogPost.getInitialProps = async ({ res, query }) => {
   // TODO: aşağıdaki satırda bulunan adresi kendi sunucu adresinle değiştirmelisin
-  const res = await fetch(`http://localhost:3000/api/post/${query.postId}`);
-  const json = await res.json();
-  return { post: json.post };
+  const response = await fetch(
+    `http://localhost:3000/api/post/${query.postId}`
+  );
+  const json = await response.json();
+  let data = {
+    post: json.post
+  };
+
+  if (Object.keys(json).length < 1) {
+    res.statusCode = 404;
+    data["statusCode"] = 404;
+  }
+
+  return data;
 };
 
 export default BlogPost;
