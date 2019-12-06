baf13f8931bdb1d2c66b1ce3967584af92527ce9
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2019-12-06 14:28:58 +0100

    add icon to the social links

--- components/header.js
+++ components/header.js
@@ -7,10 +7,25 @@ const Header = ({ minimal }) => {
       <div className="hero">
         <h1 className="hero-title">Selman Kahya</h1>
         <div className="hero-social-links">
-            <a className="social-link" href="https://medium.com/@selmankahya">Medium</a>
-            <a className="social-link" href="https://www.twitter.com/selmankahyax">Twitter</a>
-            <a className="social-link" href="https://www.linkedin.com/in/selmankahya">LinkedIn</a>
-            <a className="social-link" href="https://www.instagram.com/selmankahyax/?hl=en">Instagram</a>
+            <a className="social-link" href="https://medium.com/@selmankahya">
+              <span className="icon-facebook-squared"></span>
+              Medium
+            </a>
+            <a className="social-link" href="https://www.twitter.com/selmankahyax">
+            <span className="icon-twitter-squared"></span>
+            Twitter
+            </a>
+            <a className="social-link" href="https://www.linkedin.com/in/selmankahya">
+            <span className="icon-linkedin-squared"></span>
+
+            LinkedIn
+            </a>
+            <a className="social-link" href="https://www.instagram.com/selmankahyax/?hl=en">
+            
+            <span className="icon-instagram-filled"></span>
+
+            Instagram
+            </a>
         </div>
       </div>
     );
