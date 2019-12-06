ca4864dd7f59a5cb05a7ec84a18aca826154e4eb
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-06 23:53:12 +0300

    declare global font-family to the app and write withCSS plugin in next config for use global css files

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,9 @@
+const withCSS = require("@zeit/next-css");
+
 module.exports = {
   devIndicators: {
     autoPrerender: false
   }
 };
+
+module.exports = withCSS();
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,19 +1,26 @@
 import * as React from "react";
 import Head from "next/head";
 import Navbar from "../Components/Navbar/Navbar";
+import Home from "./Home/Home";
+import "../utils/css/index.css";
 
-const Home: React.FC = () => (
-  <div>
+const App: React.FC = () => (
+  <div className="wrapper">
     <Head>
       <title>Berkay'ın Bloğu</title>
       <link
         rel="stylesheet"
         href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
       />
+      <link
+        href="https://fonts.googleapis.com/css?family=Poppins&display=swap"
+        rel="stylesheet"
+      />
     </Head>
 
     <Navbar />
+    <Home />
   </div>
 );
 
-export default Home;
+export default App;
