1867a10ab519b333b280b142c78de24cfa348b05
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-07 00:22:13 +0300

    apollo server setup, dotenv setup, typeorm setup for creating orms and database connection

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import * as express from "express";
+import * as cors from "cors";
+import * as dotenv from "dotenv";
+import { createConnection, getMongoManager, MongoEntityManager } from "typeorm";
+import { makeExecutableSchema } from "graphql-tools";
+import { GraphQLSchema } from "graphql";
+import { importSchema } from "graphql-import";
+import { ApolloServer } from "apollo-server-express";
+// graphql resolvers
+import resolvers from "./graphql/resolvers";
+
+// dotenv setup
+dotenv.config();
+
+// graphql type definition
+const typeDefs: string = importSchema("src/graphql/schema.graphql");
+// make schema that includes our resolvers and typeDefs
+const schema: GraphQLSchema = makeExecutableSchema({ typeDefs, resolvers });
+
+// create typeorm connection that includes database connection and start apollo server
+createConnection().then((): void => {
+  const server: ApolloServer = new ApolloServer({
+    schema
+  });
+
+  const app = express();
+
+  app.use(cors());
+
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+  });
+});
