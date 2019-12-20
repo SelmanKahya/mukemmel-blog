03d1daefdd242bea43b7096b3f9be6b9cb5aaf5f
Burak <burakkayakoc@gmail.com>
2019-12-20 10:17:40 -0600

    pusher next.js config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-module.exports = {
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: "raw-loader"
-    });
 
-    return config;
-  }
-};
+    const webpack = require('webpack');
+    require('dotenv').config();
+
+    module.exports = {
+      webpack: config => {
+        const env = Object.keys(process.env).reduce((acc, curr) => {
+          acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+          return acc;
+        }, {});
+
+        config.plugins.push(new webpack.DefinePlugin(env));
+
+        return config;
+      }
+    };
