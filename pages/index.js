158b9a34cef93a0db411b601e8331a783261a5d1
Batin EVIRGEN <batinevirgen@gmail.com>
2019-12-20 19:12:01 +0300

    Changed index page to class that extends React.Component.

--- pages/index.js
+++ pages/index.js
@@ -7,28 +7,39 @@ import Meta from '../components/Meta';
 
 import NavigationBar from '../components/NavigationBar';
 
-const Home = ({ posts }) => (
-  <div className="container">
-    <Meta title="Home" />
-    <NavigationBar />
+class Home extends React.Component {
+  getInitialProps = async ({ req }) => {
+    // TODO: aşağıdaki satırda bulunan adresi kendi sunucu adresinle değiştirmelisin
+    const res = await fetch("http://localhost:3000/api/posts");
+    const json = await res.json();
+    return { posts: json.posts };
+  }
 
-    {
-    posts.map(post => (
-      <div className="blog">
-        <h2 className="blog-title">
-          <Link href={post.slug}>
-            <a className="blog-title-link">{post.title}</a>
-          </Link>
-        </h2>
-        <div className="blog-text">
-          <ReactMarkdown source={post.details} />
-        </div>
-        <div className="blog-date">{post.date}</div>
+  render() {
+    return (
+      <div className="container">
+        <Meta title="Home" />
+        <NavigationBar />
+
+        {
+        this.props.posts.map(post => (
+          <div className="blog">
+            <h2 className="blog-title">
+              <Link href={post.slug}>
+                <a className="blog-title-link">{post.title}</a>
+              </Link>
+            </h2>
+            <div className="blog-text">
+              <ReactMarkdown source={post.details} />
+            </div>
+            <div className="blog-date">{post.date}</div>
+          </div>
+        ))
+        }
       </div>
-    ))
-    }
-  </div>
-);
+    )
+  }
+}
 
 Home.getInitialProps = async ({ req }) => {
   // TODO: aşağıdaki satırda bulunan adresi kendi sunucu adresinle değiştirmelisin
