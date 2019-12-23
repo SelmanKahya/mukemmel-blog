59506ef39fcd8f66483bce005ba41a79ddc13a64
Burak <burakkayakoc@gmail.com>
2019-12-22 23:13:45 -0600

    getInitialProps from Firebase.

--- pages/index.js
+++ pages/index.js
@@ -1,87 +1,35 @@
-import React from "react";
-import fetch from "isomorphic-unfetch";
-import Head from "next/head";
-import Link from "next/link";
-import ReactMarkdown from "react-markdown";
+//import Hero from '../components/Hero.js'
+import { loadFirebase } from '../lib/db.js'
 
-const Home = ({ posts }) => (
-  <div className="container">
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+export default class Index extends React.Component {
+  static getInitialProps = () =>
+    loadFirebase().firestore().collection('Blogs')
+      .limit(10)
+      .get()
+      .then(snapshot => {
+        let data = []
+        snapshot.forEach((doc) => {
+          data.push({
+            id: doc.id,
+            ...doc.data()
+          })
+        })
+        return { Blogs: data }
+      })
 
-    <div className="hero">
-      <h1 className="hero-title">Selman Kahya</h1>
-      <div className="hero-social-links">
-        <Link href="">
-          <a className="social-link">Medium</a>
-        </Link>
-        <Link href="">
-          <a className="social-link">Twitter</a>
-        </Link>
-        <Link href="">
-          <a className="social-link">LinkedIn</a>
-        </Link>
-        <Link href="">
-          <a className="social-link">Instagram</a>
-        </Link>
-      </div>
-    </div>
-
-    {posts.map(post => (
-      <div className="blog">
-        <h2 className="blog-title">
-          <Link href={post.slug}>
-            <a className="blog-title-link">{post.title}</a>
-          </Link>
-        </h2>
-        <div className="blog-text">
-          <ReactMarkdown source={post.details} />
-        </div>
-        <div className="blog-date">{post.date}</div>
-      </div>
-    ))}
-
-    <style jsx>{`
-      .container {
-        max-width: 650px;
-        width: 100%;
-        margin: 0 auto;
+      render() {
+        const Blogs = this.props.Blogs
+        return 
+          <div id="Blogs">
+            {(Blogs && Blogs.length > 0) 
+              ? <ul>
+                {Blogs.map(blog => 
+                  <li key="{blog.id}">
+                <h3>{blog.name} has a ranking of 
+                <em>{blog.ranking}!</em></h3></li>)}
+              </ul> 
+              : <p><strong>Have nothing!!</strong></p>}
+              <hr/>
+          </div>
       }
-
-      .hero {
-        text-align: center;
-        margin: 96px 0;
-      }
-
-      .social-link {
-        margin-right: 8px;
-      }
-
-      .hero-title {
-        font-size: 48px;
-      }
-
-      .blog-date {
-        text-align: right;
-        color: #cccccc;
-        margin: 12px 0 48px 0;
-      }
-
-      a {
-        color: #35459e;
-        text-decoration: none;
-      }
-    `}</style>
-  </div>
-);
-
-Home.getInitialProps = async ({ req }) => {
-  // TODO: aşağıdaki satırda bulunan adresi kendi sunucu adresinle değiştirmelisin
-  const res = await fetch("http://localhost:3000/api/posts");
-  const json = await res.json();
-  return { posts: json.posts };
-};
-
-export default Home;
+    }
