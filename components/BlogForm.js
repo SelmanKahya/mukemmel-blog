84d9274c5e6d2bf3547c87c40f74a8e244494adc
Burak <burakkayakoc@gmail.com>
2019-12-25 19:18:10 -0600

    Install Semantic UI, working on CRUD with firebase

---
+++ components/BlogForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Segment, Form, Button } from "semantic-ui-react";
+
+class BlogForm extends Component {
+  state = {
+    Name: "",
+    Ranking: "",
+  };
+}
+
+export default BlogForm;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import { loadFirebase } from '../lib/db.js'
+import BlogForm from "../components/BlogForm";
 
 export default class extends React.Component {
+
   static getInitialProps = () =>
     loadFirebase().firestore().collection('Blogs')
       .limit(10)
@@ -16,16 +18,34 @@ export default class extends React.Component {
         return { Blogs: data }
         console.log(Blogs)
       })
+
+  state = {
+    Blogs: this.props.Blogs,
+  };
+
+
   render() {
     const Blogs = this.props.Blogs
     return (
       <React.Fragment>
+
         {Blogs.map(blog =>
-        <div key={"blog.id"}>
-          <p>Blog Name:{blog.Name}  </p>
-          <p>Blog Ranking:{blog.Ranking}</p>
+          <div key={"blog.id"}>
+            <h3>Existing Data:</h3>
+            <p>Blog Name:{blog.Name}  </p>
+            <p>Blog Ranking:{blog.Ranking}</p>
           </div>)}
+
+        <h3>Add Data</h3>
+        <div>
+          <BlogForm />
+        </div>
+
+
       </React.Fragment>
+
+
+
     );
   }
 }
